3aea848f63694a6f0546fff9cd2ba26db686d6c8
amars29 <amars@geekyants>
2021-12-28 15:16:27 +0530

    fix: Password input

--- src/nb/components/primitives/Input/Elements.tsx
+++ src/nb/components/primitives/Input/Elements.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import { Input, Icon, Stack } from 'native-base';
-import { MaterialIcons } from '@expo/vector-icons';
+import React from "react";
+import { Input, Icon, Stack } from "native-base";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export const Example = () => {
+  const [show, setShow] = React.useState(false);
   return (
     <Stack space={4} w="100%" alignItems="center">
       <Input
-        w={{ base: '75%', md: '25%' }}
+        w={{ base: "75%", md: "25%" }}
         InputLeftElement={
           <Icon
             as={<MaterialIcons name="person" />}
@@ -18,13 +19,15 @@ export const Example = () => {
         placeholder="Name"
       />
       <Input
-        w={{ base: '75%', md: '25%' }}
+        w={{ base: "75%", md: "25%" }}
+        type={show ? "text" : "password"}
         InputRightElement={
           <Icon
-            as={<MaterialIcons name="visibility-off" />}
+            as={<MaterialIcons name={show ? "visibility" : "visibility-off"} />}
             size={5}
             mr="2"
             color="muted.400"
+            onPress={() => setShow(!show)}
           />
         }
         placeholder="Password"
