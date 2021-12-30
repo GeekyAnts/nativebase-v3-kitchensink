1c210db4ef22f618a74a8e817088e310385a1399
amars29 <amars@geekyants>
2021-12-30 10:42:05 +0530

    update from storybook

--- src/nb/components/primitives/Input/Elements.tsx
+++ src/nb/components/primitives/Input/Elements.tsx
@@ -1,13 +1,12 @@
-import React from "react";
-import { Input, Icon, Stack } from "native-base";
-import { MaterialIcons } from "@expo/vector-icons";
+import React from 'react';
+import { Input, Icon, Stack } from 'native-base';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export const Example = () => {
-  const [show, setShow] = React.useState(false);
   return (
     <Stack space={4} w="100%" alignItems="center">
       <Input
-        w={{ base: "75%", md: "25%" }}
+        w={{ base: '75%', md: '25%' }}
         InputLeftElement={
           <Icon
             as={<MaterialIcons name="person" />}
@@ -19,15 +18,13 @@ export const Example = () => {
         placeholder="Name"
       />
       <Input
-        w={{ base: "75%", md: "25%" }}
-        type={show ? "text" : "password"}
+        w={{ base: '75%', md: '25%' }}
         InputRightElement={
           <Icon
-            as={<MaterialIcons name={show ? "visibility" : "visibility-off"} />}
+            as={<MaterialIcons name="visibility-off" />}
             size={5}
             mr="2"
             color="muted.400"
-            onPress={() => setShow(!show)}
           />
         }
         placeholder="Password"
