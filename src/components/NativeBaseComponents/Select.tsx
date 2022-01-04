c811ac9925170b32b0e805777f46b526bd594400
MD-REHMAN <rehman.2468@gmail.com>
2022-01-04 19:27:56 +0530

    patch: android crash fix

--- src/components/NativeBaseComponents/Select.tsx
+++ src/components/NativeBaseComponents/Select.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import { Select, Box, VStack } from 'native-base';
+import React from "react";
+import { Select, Box, VStack } from "native-base";
 
 export const Example = () => {
-  return (
-    <VStack justifyContent="center" w="80%">
-      <Select isDisabled>
-        <Select.Item label="JavaScript" value="js" />
-      </Select>
-      <Box
-        position="absolute"
-        _light={{
-          bg: 'gray.600',
-        }}
-        _dark={{
-          bg: 'gray.100',
-        }}
-        h="8px"
-        rounded="25"
-        w="40%"
-        mx={5}
-      />
-    </VStack>
-  );
+	return (
+		<VStack justifyContent="center" w="80%">
+			<Select>
+				<Select.Item label="JavaScript" value="js" />
+			</Select>
+			<Box
+				position="absolute"
+				_light={{
+					bg: "gray.600",
+				}}
+				_dark={{
+					bg: "gray.100",
+				}}
+				h="8px"
+				rounded="25"
+				w="40%"
+				mx={5}
+			/>
+		</VStack>
+	);
 };
