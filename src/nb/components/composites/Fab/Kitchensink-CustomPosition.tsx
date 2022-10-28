2d7643ac354f175ead550f5482f51255f7ef037c
amars29 <amars@geekyants>
2022-10-28 12:18:36 +0530

    upgraded nb version

--- src/nb/components/composites/Fab/Kitchensink-CustomPosition.tsx
+++ src/nb/components/composites/Fab/Kitchensink-CustomPosition.tsx
@@ -1,6 +1,6 @@
-import { Fab, Icon, NativeBaseProvider, Box } from "native-base";
-import React from "react";
-import { AntDesign } from "@expo/vector-icons";
+import { Fab, Icon, NativeBaseProvider, Box } from 'native-base';
+import React from 'react';
+import { AntDesign } from '@expo/vector-icons';
 
 export const Example = () => {
   return (
