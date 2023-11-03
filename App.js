322634bd03db2c97822085f53d2f2ebf159ba63c
Suraj <suraj@Surajs-MacBook-Pro.local>
2023-11-03 13:48:30 +0530

    fix: added gluestack

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import { NativeBaseProvider } from 'native-base';
-import { BaseTheme } from './src/theme';
-import config from './nativebase.config';
-import { Root } from './src/components/RootComponent';
+import React from "react";
+import { NativeBaseProvider, Box, Text } from "native-base";
+import { BaseTheme } from "./src/theme";
+import config from "./nativebase.config";
+import { Root } from "./src/components/RootComponent";
+import { View } from "react-native";
 
 export default function App() {
+  // return <View style={{ backgroundColor: "red", padding: 100 }}></View>;
   return (
     <NativeBaseProvider theme={BaseTheme} config={config}>
-      <Root />
+      {/* <Root /> */}
+      <Box bg="red.400" p="10">
+        <Text>Hello</Text>
+      </Box>
+      {/* <View style={{ backgroundColor: "red", padding: 100 }}></View> */}
     </NativeBaseProvider>
   );
 }
--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,14 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
       [
         "module-resolver",
         {
           alias: {
             "@native-base/icons": "@native-base/icons/lib",
+            "native-base": "@gluestack-ui/themed-native-base",
           },
         },
       ],
